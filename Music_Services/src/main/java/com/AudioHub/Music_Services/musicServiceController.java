4fc43e3fb8e4e3bf69b5da3e72f0dc2a3d36a33c
Hiransanjeewa <hiransanjeewaa@gmail.com>
2023-08-10 22:08:46 +0530

    new endpoint added

--- Music_Services/src/main/java/com/AudioHub/Music_Services/musicServiceController.java
+++ Music_Services/src/main/java/com/AudioHub/Music_Services/musicServiceController.java
@@ -6,14 +6,23 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
-@RequestMapping("api/songs/")
+@RequestMapping("api")
 public class musicServiceController {
 
-    @GetMapping(path = "getsongs")
+    @GetMapping(path = "/")
+    public String Songs(){
+        System.out.println("controller started");
+        return "All the songs are returned";
+    }
+
+    @GetMapping(path = "/songs/getsongs")
     public String GetSongs(@RequestParam Integer songid){
         System.out.println("controller started");
         return "Here is the song with the ID"+ songid;
     }
+
+
+
 
 
 }
